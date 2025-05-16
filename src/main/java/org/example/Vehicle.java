78ff8cdcddeb9fe7649c910cd3231c0df79de39f
Emiliya Ileeva <emilyileeva@gmail.com>
2025-05-16 11:56:03 -0700

    Updated Vehicle class with private variables

--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public class Vehicle {
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private double price;
+
 }
